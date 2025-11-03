82409ad5428871eed25283a0f39196789761f3d7
rajeeve-ravi-ics4u1-01-25 <rajeeve.ravi@student.tdsb.on.ca>
2025-11-03 13:33:36 -0500

    Stu submit V1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,43 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-	}
+    }
 
+    //  Q1
+    public static int lettersToFive(String word, char character) {
+        int count = 0;
+        int totalLetters = 0;
+        while (count < 5 && totalLetters < word.length()) {
+            if (word.charAt(totalLetters) == character) {
+                count++;
+            }
+            totalLetters++;
+        }
+        return count == 5 ? totalLetters : -1;
+    }
+
+    // Q2
+    public static int countLetter(String word, char character) {
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == character) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Q3
+    public static int oddSum(int num1, int num2) {
+        int sum = 0;
+        int start = Math.min(num1, num2);
+        int end = Math.max(num1, num2);
+        for (int i = start; i <= end; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
 }
